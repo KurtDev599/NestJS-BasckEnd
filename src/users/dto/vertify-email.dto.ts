47a18b3455d88cc8f95ab5e18be628b509c8a2f8
KurtDev599 <sw536880@naver.com>
2023-05-06 21:25:22 +0900

    dto생성 및 controller 동작 확인

---
+++ src/users/dto/vertify-email.dto.ts
@@ -0,0 +1,3 @@
+export class VertifyEmailDto {
+    sigupVertifyToken: string
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { VertifyEmailDto } from './dto/vertify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserInfoDto } from './dto/user-info.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfoDto> {
+    console.log(userId)
+    return;
+  }
+
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @Post('/email-vertify')
+  async vertifyEmail(@Query() dto: VertifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
   }
 }
